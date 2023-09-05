92d760150ceed26325af0f6002d54255a633a783
Harold Edsel Cabaluna <39079376+GhostPoltergeist@users.noreply.github.com>
2023-09-05 15:05:31 +0800

    Large files Excluded 😁

---
+++ java_integ/Main.java
@@ -0,0 +1,10 @@
+package EmailSender;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+
+public class Main {
+    public static void main(String[] args) {
+        FlatDarkLaf.setup();
+        EmailSender emailSender = new EmailSender();
+    }
+}
